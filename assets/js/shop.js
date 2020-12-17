1d3fce3ed9887221cf2dd4b57341a037b3c83e16
Ash066 <aisling.o.neill066@gmail.com>
2020-12-17 18:01:26 +0000

    Cart pop up created and connected to cart button, associated javascript complete

---
+++ assets/js/shop.js
@@ -0,0 +1,13 @@
+/*Display Cart*/
+(function(){
+
+    const cartInfo = document.getElementById("cart-info");
+    const cart = document.getElementById("cart");
+
+    cartInfo.addEventListener('click', function(){
+        cart.classList.toggle("show-cart");
+    })
+})();
+
+/*Add items to Cart*/
+
